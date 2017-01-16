9a04ba8ccebbe86bb5973d838782ddfcc5543e1a
Vineeth Chohan <vineeth.chohan@gmail.com>
2017-01-15 22:25:41 -0500

    Golf Travel App - 10th Commit *UI Hardening

--- app/src/main/java/com/vchohan/golftravel/RegisterActivity.java
+++ app/src/main/java/com/vchohan/golftravel/RegisterActivity.java
@@ -6,18 +6,28 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class RegisterActivity extends BaseActivity implements View.OnClickListener {
 
@@ -33,9 +43,17 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
 
     private ImageView mProfilePhoto;
 
-    private FirebaseAuth mAuth;
+    private static final int CAMERA_REQUEST = 0;
 
-    private DatabaseReference mDatabase;
+    private static final int GALLERY_REQUEST = 1;
+
+    private static final String URI = "uri";
+
+    private Uri mImageUri = null;
+
+    private String selectImageOptions;
+
+    private FirebaseAuth mAuth;
 
     private DatabaseReference mDatabaseUsers;
 
@@ -54,6 +72,21 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
         mEmailField = (EditText) findViewById(R.id.register_email_input);
         mPasswordField = (EditText) findViewById(R.id.register_password_input);
         mProfilePhoto = (ImageView) findViewById(R.id.register_profile_photo);
+        mProfilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectPhotoFrom();
+//                Intent galleryIntent = new Intent();
+//                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+//                galleryIntent.setType("image/*");
+//                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+
+        if (savedInstanceState != null) {
+            mImageUri = savedInstanceState.getParcelable(URI);
+            mProfilePhoto.setImageURI(mImageUri);
+        }
 
         findViewById(R.id.register_button).setOnClickListener(this);
 
@@ -63,11 +96,9 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
     }
 
     private void initializeFirebaseAuth() {
-        // [START initialize_auth]
         mAuth = FirebaseAuth.getInstance();
-        // [END initialize_auth]
-
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mStorage = FirebaseStorage.getInstance().getReference().child("ProfilePhotos");
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
     }
 
     private void createAccount(final String firstName, final String lastName, String email, String password) {
@@ -92,10 +123,10 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
                     // signed in user can be handled in the listener.
                     if (!task.isSuccessful()) {
                         Toast.makeText(RegisterActivity.this, R.string.auth_failed, Toast.LENGTH_SHORT).show();
-                    } else                     if (task.isSuccessful()) {
+                    } else if (task.isSuccessful()) {
                         String userId = mAuth.getCurrentUser().getUid();
 
-                        DatabaseReference currentUserDatabase = mDatabase.child(userId);
+                        DatabaseReference currentUserDatabase = mDatabaseUsers.child(userId);
                         currentUserDatabase.child("firstName").setValue(firstName);
                         currentUserDatabase.child("lastName").setValue(lastName);
                         currentUserDatabase.child("image").setValue("default");
@@ -106,13 +137,9 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
                         mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(mainIntent);
                     }
-
-                    // [START_EXCLUDE]
                     hideProgressDialog();
-                    // [END_EXCLUDE]
                 }
             });
-        // [END create_user_with_email]
     }
 
     private boolean validateForm() {
@@ -154,6 +181,136 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
                 mLastName.getText().toString().trim(),
                 mEmailField.getText().toString().trim(),
                 mPasswordField.getText().toString().trim());
+        }
+    }
+
+    private void selectPhotoFrom() {
+        final CharSequence[] items = {"Take Photo", "Choose from Library", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = CameraUtility.checkPermission(RegisterActivity.this);
+                if (items[item].equals("Take Photo")) {
+                    selectImageOptions = "Take Photo";
+                    if (result) {
+                        cameraIntent();
+                    }
+                } else if (items[item].equals("Choose from Library")) {
+                    selectImageOptions = "Choose from Library";
+                    if (result) {
+                        galleryIntent();
+                    }
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == GALLERY_REQUEST) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Now user should be able to use camera
+                switch (requestCode) {
+                    case CameraUtility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            if (selectImageOptions.equals("Take Photo")) {
+                                cameraIntent();
+                            } else if (selectImageOptions.equals("Choose from Library")) {
+                                galleryIntent();
+                            }
+                        } else {
+                            //code for deny
+                        }
+                        break;
+                }
+            } else {
+                // Your app will not have this permission.
+                String errorText = "Permission was denied to access your camera, please visit app settings to allow permission";
+                Toast.makeText(this, errorText, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void cameraIntent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.CAMERA}, GALLERY_REQUEST);
+            }
+        }
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    private void galleryIntent() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+//            mImageUri = data.getData();
+//            CropImage.activity(mImageUri)
+//                .setGuidelines(CropImageView.Guidelines.ON)
+//                .setCropShape(CropImageView.CropShape.OVAL)
+//                .setAspectRatio(1, 1)
+//                .start(this);
+//        }
+//
+//        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+//            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+//            if (resultCode == RESULT_OK) {
+//                Uri resultUri = result.getUri();
+//                Glide.with(getApplicationContext()).load(resultUri)
+//                    .crossFade()
+//                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                    .into(mProfilePhoto);
+//
+//                // Loading profile image
+//                Glide.with(getApplicationContext()).load(resultUri)
+//                    .crossFade()
+//                    .thumbnail(0.5f)
+//                    .bitmapTransform(new CircleTransform(getApplicationContext()))
+//                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                    .into(mProfilePhoto);
+//
+//
+//            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+//                Exception error = result.getError();
+//            }
+//        }
+
+        if (requestCode == GALLERY_REQUEST || requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            if (data != null) {
+                try {
+                    mImageUri = data.getData();
+                    CropImage.activity(mImageUri)
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setCropShape(CropImageView.CropShape.OVAL)
+                        .setAspectRatio(1, 1)
+                        .start(this);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                mProfilePhoto.setImageURI(mImageUri);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mProfilePhoto != null) {
+            outState.putParcelable(URI, mImageUri);
         }
     }
 }

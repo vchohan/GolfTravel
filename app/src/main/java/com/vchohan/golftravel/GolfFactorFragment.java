2e161290c414420d9b9b5797bac10fb7ee89e95b
Vineeth Chohan <vineeth.chohan@gmail.com>
2017-02-07 15:54:53 -0500

    Golf Travel App - 27th Commit *UI Hardening

--- app/src/main/java/com/vchohan/golftravel/GolfFactorFragment.java
+++ app/src/main/java/com/vchohan/golftravel/GolfFactorFragment.java
@@ -2,11 +2,7 @@ package com.vchohan.golftravel;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,32 +11,29 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.vchohan.baseui.CustomGauge;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class GolfFactorFragment extends Fragment implements View.OnClickListener {
 
     public static final String TAG = GolfFactorFragment.class.getSimpleName();
 
-    private CustomGauge mWeatherGauge, mGolfFactorGauge;
+    private RelativeLayout mAddGolfRoundInfoButton;
 
-    private TextView mWeatherText, mGolfFactorText;
+    private LinearLayout mViewGolfRoundInfoButton, mGolfRoundInfoLayout;
+
+    private CustomGauge mGolfFactorGauge;
+
+    private TextView mGolfFactorText;
 
     int i;
-
-    private LinearLayout mSetGolfFactorButton, mExpandLayout, mAddGolfRoundInfoButton;
 
     private boolean isExpanded = false;
 
     private ImageView mImageToggle;
 
-    private TabLayout mTabLayout;
-
-    private ViewPager mViewPager;
 
     public GolfFactorFragment() {
         // Required empty public constructor
@@ -58,32 +51,21 @@ public class GolfFactorFragment extends Fragment implements View.OnClickListener
         View rootView = inflater.inflate(R.layout.golf_factor_fragment, container, false);
         setupGaugeView(rootView);
 
-        mSetGolfFactorButton = (LinearLayout) rootView.findViewById(R.id.set_golf_factor_button);
-        mSetGolfFactorButton.setOnClickListener(this);
-        mExpandLayout = (LinearLayout) rootView.findViewById(R.id.golf_factor_expandable_Layout);
-        mImageToggle = (ImageView) rootView.findViewById(R.id.toggle_up_down_view);
-        mImageToggle.setImageResource(R.drawable.ic_keyboard_arrow_down_white_24dp);
 
-        mAddGolfRoundInfoButton = (LinearLayout) rootView.findViewById(R.id.add_golf_round_info_button);
+        mAddGolfRoundInfoButton = (RelativeLayout) rootView.findViewById(R.id.add_golf_round_info_button);
         mAddGolfRoundInfoButton.setOnClickListener(this);
 
-        mViewPager = (ViewPager) rootView.findViewById(R.id.golf_factor_view_pager);
-        setupViewPager(mViewPager);
-
-        mTabLayout = (TabLayout) rootView.findViewById(R.id.golf_factor_tab_view);
-        mTabLayout.setupWithViewPager(mViewPager);
+        mViewGolfRoundInfoButton = (LinearLayout) rootView.findViewById(R.id.view_golf_round_info_button);
+        mViewGolfRoundInfoButton.setOnClickListener(this);
+        mGolfRoundInfoLayout = (LinearLayout) rootView.findViewById(R.id.golf_fround_info_Layout);
+        mImageToggle = (ImageView) rootView.findViewById(R.id.toggle_up_down_view);
+        mImageToggle.setImageResource(R.drawable.ic_keyboard_arrow_down_white_24dp);
 
         return rootView;
     }
 
     private void setupGaugeView(View rootView) {
-
-        mWeatherGauge = (CustomGauge) rootView.findViewById(R.id.weather_gauge);
         mGolfFactorGauge = (CustomGauge) rootView.findViewById(R.id.golf_factor_gauge);
-
-        mWeatherText = (TextView) rootView.findViewById(R.id.weather_text);
-        mWeatherText.setText(Integer.toString(mWeatherGauge.getValue()));
-
         mGolfFactorText = (TextView) rootView.findViewById(R.id.golf_factor_text);
         mGolfFactorText.setText(Integer.toString(mGolfFactorGauge.getValue()));
 
@@ -95,11 +77,7 @@ public class GolfFactorFragment extends Fragment implements View.OnClickListener
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-
-                                    mWeatherGauge.setValue(i);
                                     mGolfFactorGauge.setValue(i);
-
-                                    mWeatherText.setText(Integer.toString(mWeatherGauge.getValue()));
                                     mGolfFactorText.setText(Integer.toString(mGolfFactorGauge.getValue()));
                                 }
                             });
@@ -117,7 +95,7 @@ public class GolfFactorFragment extends Fragment implements View.OnClickListener
     @Override
     public void onClick(final View v) {
         switch (v.getId()) {
-            case R.id.set_golf_factor_button:
+            case R.id.view_golf_round_info_button:
                 setGolfFactorView();
                 break;
             case R.id.add_golf_round_info_button:
@@ -138,25 +116,25 @@ public class GolfFactorFragment extends Fragment implements View.OnClickListener
     }
 
     public void animateExpandLayout() {
-        if (mExpandLayout != null) {
+        if (mGolfRoundInfoLayout != null) {
             isExpanded = true;
-            mExpandLayout.setVisibility(LinearLayout.VISIBLE);
+            mGolfRoundInfoLayout.setVisibility(LinearLayout.VISIBLE);
             Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.jump_from_down);
             animation.setDuration(500);
-            mExpandLayout.setAnimation(animation);
-            mExpandLayout.animate();
+            mGolfRoundInfoLayout.setAnimation(animation);
+            mGolfRoundInfoLayout.animate();
             animation.start();
         }
     }
 
     public void animateCollapseLayout() {
-        if (mExpandLayout != null) {
+        if (mGolfRoundInfoLayout != null) {
             isExpanded = false;
-            mExpandLayout.setVisibility(LinearLayout.GONE);
+            mGolfRoundInfoLayout.setVisibility(LinearLayout.GONE);
             Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.jump_to_down);
             animation.setDuration(500);
-            mExpandLayout.setAnimation(animation);
-            mExpandLayout.animate();
+            mGolfRoundInfoLayout.setAnimation(animation);
+            mGolfRoundInfoLayout.animate();
             animation.start();
         }
     }
@@ -175,42 +153,4 @@ public class GolfFactorFragment extends Fragment implements View.OnClickListener
 
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
-        adapter.addFragment(new DateFragment(), "DATE");
-        adapter.addFragment(new TimeFragment(), "TIME");
-        adapter.addFragment(new CourseFragment(), "COURSE");
-        viewPager.setAdapter(adapter);
-    }
-
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
-    }
 }
--- app/src/main/java/com/vchohan/golftravel/TeeTimeFragment.java
+++ app/src/main/java/com/vchohan/golftravel/TeeTimeFragment.java
@@ -2,19 +2,26 @@ package com.vchohan.golftravel;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-/**
- * Created by vchohan on 2/1/17.
- */
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeeTimeFragment extends Fragment implements View.OnClickListener {
 
     public static final String TAG = TeeTimeFragment.class.getSimpleName();
+
+    private TabLayout mTabLayout;
+
+    private ViewPager mViewPager;
 
     private LinearLayout mBookTeeTimeButton;
 
@@ -33,10 +40,55 @@ public class TeeTimeFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.tee_time_fragment, container, false);
 
+        mViewPager = (ViewPager) rootView.findViewById(R.id.golf_factor_view_pager);
+        setupViewPager(mViewPager);
+
+        mTabLayout = (TabLayout) rootView.findViewById(R.id.golf_factor_tab_view);
+        mTabLayout.setupWithViewPager(mViewPager);
+
         mBookTeeTimeButton = (LinearLayout) rootView.findViewById(R.id.book_tee_time_button);
         mBookTeeTimeButton.setOnClickListener(this);
 
         return rootView;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new DateFragment(), "DATE");
+        adapter.addFragment(new TimeFragment(), "TIME");
+        adapter.addFragment(new CourseFragment(), "COURSE");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
     @Override
